72f6f450592b2b3adfd4e882298fb652718b951a
Felipe Pinheiro <flppinheiro@hotmail.com>
2023-08-04 20:56:17 -0300

    desafio 001

--- desafios/d001/resp.php
+++ desafios/d001/resp.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Resposta 001</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <main>
@@ -12,16 +13,18 @@
         </header>
         <article>
             <?php 
-                $numero = $_GET["numero"];
+                $numero = $_GET["numero"] ?? 1;
                 $antecessor = $numero - 1;
                 $sucessor = $numero + 1;
 
                 echo "<p> Você entrou com o número <strong>$numero.</strong> </p>";
                 echo "<p> O seu Antecessor é <strong> $antecessor. </strong></p>";
                 echo "<p> O seu Sucessor é <strong> $sucessor. </strong> </p>";
-
             ?>
+            <h3><a href="index.html">Voltar</a></h3>
+            
         </article>
+        
     </main>
 </body>
 </html>
